5d718ed2a8e745e3597f22bf26a64cc28c10f6de
Matthew Kastor <matthewkastor@gmail.com>
2013-09-29 01:13:00 -0400

    regenerated from source.

--- docs/vsdoc/symbols/atropa-vsdoc.js
+++ docs/vsdoc/symbols/atropa-vsdoc.js
@@ -6,6 +6,12 @@
 
     window.atropa = {
         /// <summary></summary>
+        /// <field name="regex" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-regex/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
+        /// <field name="arrays" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-arrays/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
         /// <field name="string" type="">A few utilities for manipulating strings.</field>
         /// <returns type="atropa"/>
                 
